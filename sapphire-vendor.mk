299273fab6880a6a2c17570eeeac78a42b77da41
Liekoo <veyeka1@gmail.com>
2025-09-25 09:25:58 +0200

    sapphire: fix and move NFC to different fragment and enable only on topaz {n7n, n7n_ss}

--- sapphire-vendor.mk
+++ sapphire-vendor.mk
@@ -11,6 +11,16 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sapphire/proprietary/odm/etc/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_product_strategies.xml \
     vendor/xiaomi/sapphire/proprietary/odm/etc/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_stream_volumes.xml \
     vendor/xiaomi/sapphire/proprietary/odm/etc/camera/camerabooster.json:$(TARGET_COPY_OUT_ODM)/etc/camera/camerabooster.json \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.hce.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.hcef.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.uicc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.se.omapi.uicc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.hce.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.hcef.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.uicc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.se.omapi.uicc.xml \
     vendor/xiaomi/sapphire/proprietary/system_ext/etc/permissions/audiosphere.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/audiosphere.xml \
     vendor/xiaomi/sapphire/proprietary/vendor/bin/STFlashTool:$(TARGET_COPY_OUT_VENDOR)/bin/STFlashTool \
     vendor/xiaomi/sapphire/proprietary/vendor/bin/adsprpcd:$(TARGET_COPY_OUT_VENDOR)/bin/adsprpcd \
@@ -787,6 +797,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sapphire/proprietary/vendor/lib64/libsnsdiaglog.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsnsdiaglog.so \
     vendor/xiaomi/sapphire/proprietary/vendor/lib64/libssc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libssc.so \
     vendor/xiaomi/sapphire/proprietary/vendor/lib64/libssc_default_listener.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libssc_default_listener.so \
+    vendor/xiaomi/sapphire/proprietary/vendor/lib64/libstfactory-vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstfactory-vendor.so \
     vendor/xiaomi/sapphire/proprietary/vendor/lib64/libswregistrationalgo.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libswregistrationalgo.so \
     vendor/xiaomi/sapphire/proprietary/vendor/lib64/libsysmon_cdsp_skel.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsysmon_cdsp_skel.so \
     vendor/xiaomi/sapphire/proprietary/vendor/lib64/libsystem_health_mon.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsystem_health_mon.so \
@@ -815,8 +826,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libar-pal \
-    libadsp_default_listener \
-    libadsprpc \
     libagm \
     libar-gsl \
     liblmthermallistner \
