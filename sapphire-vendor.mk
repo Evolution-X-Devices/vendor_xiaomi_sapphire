4e276e7f6ce0d00b3133e90da5fc1dd66357f325
Liekoo <lycoreyes099@gmail.com>
2025-12-28 03:49:13 +0000

    sapphire: update blobs to 0S2.0.207.0.VNGMIXM

--- sapphire-vendor.mk
+++ sapphire-vendor.mk
@@ -11,6 +11,16 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sapphire/proprietary/odm/etc/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_product_strategies.xml \
     vendor/xiaomi/sapphire/proprietary/odm/etc/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_stream_volumes.xml \
     vendor/xiaomi/sapphire/proprietary/odm/etc/camera/camerabooster.json:$(TARGET_COPY_OUT_ODM)/etc/camera/camerabooster.json \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.hce.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.hcef.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.uicc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.nfc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n/android.hardware.se.omapi.uicc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.hce.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.hcef.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.uicc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.nfc.xml \
+    vendor/xiaomi/sapphire/proprietary/odm/etc/permissions/sku_n7n_ss/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_n7n_ss/android.hardware.se.omapi.uicc.xml \
     vendor/xiaomi/sapphire/proprietary/system_ext/etc/permissions/audiosphere.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/audiosphere.xml \
     vendor/xiaomi/sapphire/proprietary/vendor/bin/STFlashTool:$(TARGET_COPY_OUT_VENDOR)/bin/STFlashTool \
     vendor/xiaomi/sapphire/proprietary/vendor/bin/adsprpcd:$(TARGET_COPY_OUT_VENDOR)/bin/adsprpcd \
