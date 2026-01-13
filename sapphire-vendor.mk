b5248e063bf1cdb86ccd1b6c083cfaac099dc5b1
Liekoo <veyeka1@gmail.com>
2026-01-13 12:55:34 +0000

    sapphire: Update to version OS2.0.208.0.VNGMIXM

--- sapphire-vendor.mk
+++ sapphire-vendor.mk
@@ -825,14 +825,14 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sapphire/proprietary/vendor/lib64/vendor.qti.hardware.sensorscalibrate@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.sensorscalibrate@1.0.so
 
 PRODUCT_PACKAGES += \
-    libar-pal \
+    libar-pal_vendor \
     libadsp_default_listener \
     libadsprpc \
     libagm \
-    libar-gsl \
+    libar-gsl_vendor \
     liblmthermallistner \
-    liblx-osal \
+    liblx-osal_vendor \
     libthermalclient \
     vendor.qti.hardware.AGMIPC@1.0-impl \
-    vendor.qti.hardware.pal@1.0-impl \
+    vendor.qti.hardware.pal@1.0-impl_vendor \
     audiosphere
